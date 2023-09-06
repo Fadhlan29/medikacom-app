7190177df3f60760fc5290f49565ec06fe1e5fc2
unknown <tifalrizkyarjuna@gmail.com>
2023-09-06 09:36:00 +0700

    update profile

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,12 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {View, Text, SafeAreaView, ScrollView, Image} from 'react-native';
 import React from 'react';
 import {Appbar} from 'react-native-paper';
 import Colors from '../constant/Colors';
 import {styles} from '../styles/screenStyle/ProfileStyle';
 ('../styles/screenStyle/ProfileStyle.js');
 import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+import { Link } from 'native-base';
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
@@ -12,6 +14,18 @@ const ProfileScreen = () => {
   const handleToBack = () => {
     navigation.navigate('home');
   };
+  const [kategoriSeleksi, setKategoriSeleksi] = useState([
+    {
+      author: 'VISI',
+      text: 'Menjadi SMK Unggulan yang membangun dan mengembangkan generasi muda Indonesia, khususnya Jawa Barat, menjadi manusia cerdas berakhlak mulia, produktif, terampil dan unggul memasuki pasar kerja lokal dan global diberbagai Industri dan Usaha.',
+      image: require('../assets/images/medikacomBanner.jpg'),
+    },
+    {
+      author: 'MISI',
+      text: 'Menggali dan mengembangkan potensi setiap siswa untuk memberdayakan (Empowering) kecerdasan intelektual (IQ) emosional (EQ) maupun spiritual (SQ) untuk meraih masa depan yang gemilang.',
+      image: require('../assets/images/medikacomBanner.jpg'),
+    },
+  ]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -22,6 +36,33 @@ const ProfileScreen = () => {
             <Appbar.Content title="profile" />
           </Appbar.Header>
         </View>
+        <View style={styles.banner} />
+        <View style={styles.roundedBorder} />
+        
+        <View>
+          <Image
+            style={styles.image}
+            source={require('../assets/images/medikacom.png')}
+          />
+          <Text style={styles.title}>SMK MedikaCom</Text>
+          <Text style={styles.titleSecond}>Jl. Rancabolang Soekarno-Hatta No. 10B, Manjah Lega, Rancasari, Bandung, Jawa Barat</Text>
+       </View>
+        
+        <View style={{marginTop:20}}>
+          {kategoriSeleksi.map((kategoriSeleksi, i) => (
+            <View key={i} style={styles.card}>
+              <Image style={styles.imageCard}
+                source={kategoriSeleksi.image}/>
+              <View style={styles.textCard}>
+                <Text style={{fontWeight: 'bold', fontSize: 15}}>
+                  {kategoriSeleksi.author}
+                </Text>
+                <Text>{kategoriSeleksi.text}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      
       </ScrollView>
     </SafeAreaView>
   );
--- src/styles/screenStyle/ProfileStyle.js
+++ src/styles/screenStyle/ProfileStyle.js
@@ -5,5 +5,61 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.lightGrey
+    },
+    banner: {
+        flex:1, 
+        padding:60, 
+        backgroundColor: Colors.primary, 
+        borderBottomRightRadius:20,
+        borderBottomLeftRadius:20,
+    },
+    image: {
+        marginTop:-140,
+        alignSelf:'center',
+        width:130,
+        height:130
+    },
+    title: {
+        marginTop:5,
+        fontSize: 20,
+        fontFamily: 'Poppins-SemiBold',
+        color: Colors.darkGrey,
+        alignSelf:'center',
+    },
+    roundedBorder: {
+        marginTop:-80,
+        width: 155,
+        height: 155,
+        backgroundColor: Colors.lightGrey,
+        alignSelf: 'center',
+        borderRadius:80
+    },
+    titleSecond: {
+        fontSize: 15,
+        fontFamily: 'Poppins-light',
+        color: Colors.darkGrey,
+        alignSelf:'center',
+    },
+    card: {
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        width: '100%',
+        marginBottom: 10,
+        backgroundColor: Colors.lightGrey,
+        elevation: 4,
+        flexDirection: 'row',
+    },
+    imageCard: {
+        width:150,
+        height:150,
+        resizeMode: 'cover',
+        alignSelf:'center'
+    },
+    textCard: {
+        maxWidth: 250, 
+        justifyContent:'center', 
+        alignItems:'center',
+        padding:5
     }
 })
